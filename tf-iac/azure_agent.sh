23656c871ebefce4a1f90128d0180a22d8660f9b
Dragan Tomasevic <dragan.tomasevic@vegait.rs>
2023-04-06 12:05:23 +0200

    added sleep to a script

--- tf-iac/azure_agent.sh
+++ tf-iac/azure_agent.sh
@@ -9,7 +9,6 @@ wget https://vstsagentpackage.azureedge.net/agent/3.218.0/vsts-agent-linux-x64-3
 mkdir $AGENT_PATH
 cd $AGENT_PATH
 tar xvfz ../vsts-agent-linux-x64-3.218.0.tar.gz
+echo "sleep 10" >> config.sh
 ./config.sh --unattended --url https://dev.azure.com/dragantomasevic0768 --auth pat --token $TOKEN --pool default --agent myLinuxAgent_$DATE --acceptTeeEula
-echo "Sleeping 10 sec"
-sleep 10
 ./run.sh &
